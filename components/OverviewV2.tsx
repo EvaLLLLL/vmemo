cb21fd2b7985fa99f16904dafcb1653835fc1f56
lyh <liuyuhao0414@gmail.com>
2024-11-25 15:17:19 +0800

    fix overview style

--- components/OverviewV2.tsx
+++ components/OverviewV2.tsx
@@ -62,7 +62,7 @@ export function ActivityCart() {
   }, [memoryList])
 
   return (
-    <Card>
+    <Card className="w-full">
       <CardHeader className="flex flex-col items-stretch space-y-0 border-b p-0 sm:flex-row">
         <div className="flex flex-1 flex-col justify-center gap-1 px-6 py-5 sm:py-6">
           <CardTitle>Number of Saved or Memorized Words Chart</CardTitle>
