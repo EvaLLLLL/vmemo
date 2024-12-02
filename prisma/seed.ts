0b7f8dfef65c160e111a3a59b2886c375d2150d7
lyh <liuyuhao0414@gmail.com>
2024-12-02 22:24:03 +0800

    chore: seed.js -> seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,10 @@
-const fs = require('fs')
-const path = require('path')
-const Papa = require('papaparse')
-const { PrismaClient } = require('@prisma/client')
+import fs from 'fs'
+import path from 'path'
+import { Ecdict, PrismaClient } from '@prisma/client'
+import Papa from 'papaparse'
 
 const prisma = new PrismaClient()
-const parseCSV = async (filePath) => {
+const parseCSV = async (filePath: string) => {
   const csvFile = fs.readFileSync(path.resolve(filePath), 'utf-8')
 
   return new Promise((resolve) => {
@@ -21,7 +21,7 @@ async function seed() {
   const data = await parseCSV('prisma/ecdict.csv')
   await prisma.ecdict.deleteMany()
   await prisma.ecdict.createMany({
-    data: data,
+    data: data as Ecdict[],
     skipDuplicates: true
   })
 }
