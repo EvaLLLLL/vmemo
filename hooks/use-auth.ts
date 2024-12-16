1bf8053d0fd1b68b443d570538d0a1eddb56bfa5
lyh <liuyuhao0414@gmail.com>
2024-12-16 19:29:06 +0800

    fix: fix use-auth

--- hooks/use-auth.ts
+++ hooks/use-auth.ts
@@ -1,17 +1,21 @@
 import { useQuery } from '@tanstack/react-query'
 import { useSession } from 'next-auth/react'
 import { AuthServices } from '@/lib/services'
+import { useMemo } from 'react'
 
 export function useAuth() {
   const { status } = useSession()
 
-  const { data: user, isLoading } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: [AuthServices.getUser.key],
-    queryFn: AuthServices.getUser.fn,
-    select: (data) => data.data
+    queryFn: AuthServices.getUser.fn
   })
 
-  const isAuthenticated = status === 'authenticated'
+  const isAuthenticated = useMemo(() => status === 'authenticated', [status])
+  const user = useMemo(
+    () => (isAuthenticated ? data?.data : null),
+    [isAuthenticated, data]
+  )
 
   return {
     isAuthenticated,
